9684a1f06ee6d86678ccf747f6291681b5914079
Raksha Gala <70347661+rakshargala@users.noreply.github.com>
2020-10-15 22:51:16 -0400

    Operators and if else and switch cases mixmatch

--- Hello/src/MySweetProgram.java
+++ Hello/src/MySweetProgram.java
@@ -4,88 +4,61 @@ import java.util.Scanner;
    public class MySweetProgram {
 	
 	 public static void main(String[] args) {
-
-					
-	int a = 5;
-	int b = 8;
-	int addition = a + b;
-	int subtraction = b - a;
-	int multiplication = a * b;
-	int division = b / a;
-	int modulus = b % a;
-	System.out.println("Addition of a and b is " +addition);
-	System.out.println("Subtraction of a and b is " +subtraction);
-	System.out.println("Multiplication of a and b is " +multiplication);
-	System.out.println("Division of a and b is " +division);
-	System.out.println("Remainder of a and b is " +modulus);
-	
-	
-	a+=b;
-	System.out.println("Updated value of a is " +a);
-	
-	b*=b;
-	System.out.println("Updated value of b is " +b);
-	
-	
-	
-	int x = 9;
-	int y = 8;
-	int z = 20;
-	
-	if(x > y && y >= z)
-	{
-		System.out.println("x and y are greater than z");
-	}
+		int result;			
+		Scanner input = new  Scanner(System.in);
 		
-	else if(x >= y || y > z)
-		{
+		System.out.println("Choose an operator: +, -, *, /: ");
+		String operator = input.nextLine();
+		
+		System.out.println("Enter the 1st number: ");
+		int number1 = input.nextInt();
+		
+		System.out.println("Enter the 2nd number: ");
+		int number2 = input.nextInt();
+		
+		
+		if (number1 > number2) {
+		
+		
+		switch(operator) {
+		
+		case "+":
+			result = number1 + number2;
+			System.out.println(number1 + " + " + number2 + " = " + result);
+			break;
+		
+		case "-":
+			result = number1 - number2;
+			System.out.println(number1 + " - " + number2 + " = " + result);	
+			break;
+		
 			
-			System.out.println("Either x or y is greater than z");
 			
-		}
-	
+		case "*":
+			result = number1 * number2;
+			System.out.println(number1 + " * " + number2 + " = " + result);	
+			break;
+			
+		case "/":
+			result = number1 / number2;
+			System.out.println(number1 + " / " + number2 + " = " + result);	
+			break;
+			
+		default:
+			System.out.println("Invalid operator");
+			break;
+				 }
+		
+				}	
+
+		
+		
 		else
-		{
-		 
-		System.out.println("z is greater"); 
+			System.out.println("number 2 is greater");
+	
 		
-		}
-	
-	
-	
-	
-	
-	int i;
-	for(i = 0; i <= 9; i++)
-	 {
-	   System.out.println(i);
-	 }
-	
-	
-	
-	
-	
-	//Ternary Operator
-	int r = 30;
-	int g = 98;
-	int c = (r > g)? 420:520;
-	System.out.println(c);
-	
-	
-	
-	//Bitwise Operator
-	int j = 5;
-	int k = 12;
-	int l, u, v;
-	l = j ^ k;
-	u = ~ j;
-	v = j >> 3;
-	System.out.println(l);
-	System.out.println(u);
-	System.out.println(v);
-
-	
-	
-
+		
+		input.close();
+		
   }
 }
