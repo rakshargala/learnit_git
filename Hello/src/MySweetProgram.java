d2e1f55160b638c7b37eecb7d66fff695eb881cf
Raksha Gala <raksha@nmsu.edu>
2021-06-14 18:48:34 -0400

    test SSH connection

--- Hello/src/MySweetProgram.java
+++ Hello/src/MySweetProgram.java
@@ -1,64 +1,21 @@
 
 import java.util.Scanner;
 
-   public class MySweetProgram {
-	
-	 public static void main(String[] args) {
-		int result;			
-		Scanner input = new  Scanner(System.in);
-		
-		System.out.println("Choose an operator: +, -, *, /: ");
-		String operator = input.nextLine();
-		
-		System.out.println("Enter the 1st number: ");
-		int number1 = input.nextInt();
-		
-		System.out.println("Enter the 2nd number: ");
-		int number2 = input.nextInt();
-		
-		
-		if (number1 > number2) {
-		
-		
-		switch(operator) {
-		
-		case "+":
-			result = number1 + number2;
-			System.out.println(number1 + " + " + number2 + " = " + result);
-			break;
-		
-		case "-":
-			result = number1 - number2;
-			System.out.println(number1 + " - " + number2 + " = " + result);	
-			break;
-		
-			
-			
-		case "*":
-			result = number1 * number2;
-			System.out.println(number1 + " * " + number2 + " = " + result);	
-			break;
-			
-		case "/":
-			result = number1 / number2;
-			System.out.println(number1 + " / " + number2 + " = " + result);	
-			break;
-			
-		default:
-			System.out.println("Invalid operator");
-			break;
-				 }
-		
-				}	
+public class MySweetProgram {
 
-		
-		
-		else
-			System.out.println("number 2 is greater");
-	
-		
-		
-		input.close();
-		
-  }
+
+public static void main (String[] args){
+
+Scanner input = new Scanner(System.in);
+
+System.out.println("Enter the value of character:");
+ char c = input.next().charAt(1);
+
+
+System.out.println("The value entered is " +c);
+
 }
+
+}
+
+
