9504ebfd810e91954809ee6f020977a9d4b20fd3
Raksha Gala <raksha@nmsu.edu>
2021-06-14 10:07:42 -0400

    init commit

---
+++ src/Lamp.java
@@ -0,0 +1,18 @@
+class Lamp{
+	public void Animal() {
+		System.out.println("I can eat");
+	}
+}
+
+class Dog extends Lamp{
+	@Override
+	public void Animal() {
+		super.Animal();
+		System.out.println("I cannot eat");
+	}
+}
+
+	
+	
+	
+	
---
+++ src/MySweetProgram.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class MySweetProgram {
+
+
+public static void main (String[] args){
+
+Scanner input = new Scanner(System.in);
+
+System.out.println("Enter the value of character:");
+ char c = input.next().charAt(1);
+
+
+System.out.println("The value entered is " +c);
+
+}
+
+}
+
+
